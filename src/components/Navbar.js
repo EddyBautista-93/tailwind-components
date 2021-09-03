0b56014454f3d62aada0e800f4fc7026bde2c0dd
EddyBautista <bautista.j.eddy@gmail.com>
2021-09-03 06:15:35 -0500

    making cosmetic changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,15 @@ const Navbar = () => {
     return (
         <nav className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono'
         role='navigation'>
-            <Link to='/' className='pl-8'>Blog</Link>
+            <Link to='/' className='pl-8'>
+            <svg className="w-6 h-6" data-darkreader-inline-fill="" fill="none" stroke="currentColor"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+            </svg>
+            </Link>
             <div className='px-4 cursor-pointer md:hidden'>
-                <svg className="w-6 h-6" fill='none' stroke="currentColor" viewBox=" 0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16m4 12h16m-7 6h7" />
-                </svg>
+            <svg className="w-6 h-6"  fill="none" stroke="currentColor"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
             </div>
             <div className='pr-8 md:block hidden'>
                 <Link to='/' className='p-4'>Home</Link>
@@ -20,5 +24,6 @@ const Navbar = () => {
         </nav>
     )
 }
+
 
 export default Navbar
--- src/components/hero.js
+++ src/components/hero.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom'
 const Hero = () => {
     return (
         <div className="bg-white h-screen flex flex-col justify-center items-center">
-            <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
-                <Link className="py-6 px-10 bg-yellow-500 rounded-full- text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce" to='/menu'>
-                    Order Now(' ')
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
+            <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14"> Welcome to my Blog Styled with Tailwind and created with React!</h1>
+                <Link className="py-6 px-10 rounded-full- text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce" to='/menu'>
+                <svg className="w-16 h-16"  fill="none" stroke="currentColor"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>
                 </Link>
-            </h1>
         </div>
     );
 };
+
+
 
 export default Hero;
